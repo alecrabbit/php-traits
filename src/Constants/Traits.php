cda9efc089744eeeef879612960f99f90305222e
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-01-07 13:16:22 +0200

    mod

---
+++ src/Constants/Traits.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: alec
+ * Date: 03.12.18
+ * Time: 23:55
+ */
+
+// @codeCoverageIgnoreStart
+namespace AlecRabbit\Constants\Traits;
+
+define(__NAMESPACE__ . '\DEFAULT_NAME', 'default_name');
