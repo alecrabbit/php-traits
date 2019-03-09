fc2f6761abe22f1d849f39f203e783da1c18d588
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-03-09 12:43:47 +0200

    add new traits
    Startable, Stoppable, StartableAndStoppable

---
+++ tests/HasTraitStartableAndStoppable.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Traits;
+
+use AlecRabbit\Traits\StartableAndStoppable;
+
+class HasTraitStartableAndStoppable
+{
+    use StartableAndStoppable;
+}
---
+++ tests/Unit/StoppableTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Traits;
+
+use PHPUnit\Framework\TestCase;
+
+class StoppableTest extends TestCase
+{
+    /** @var HasTraitStoppable */
+    private $obj;
+
+    /** @test */
+    public function check(): void
+    {
+        $this->assertFalse($this->obj->isStopped());
+        $this->assertTrue($this->obj->isNotStopped());
+        $this->obj->stop();
+        $this->assertTrue($this->obj->isStopped());
+        $this->assertFalse($this->obj->isNotStopped());
+    }
+
+    protected function setUp()
+    {
+        $this->obj = new HasTraitStoppable();
+    }
+}
