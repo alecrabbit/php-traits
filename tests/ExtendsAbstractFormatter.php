c11c60e4b1d8dfbe242077de5bb5711d5b0843b7
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-26 14:56:53 +0300

    abstract formatter tests

---
+++ tests/ExtendsAbstractFormatter.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Traits;
+
+use AlecRabbit\Traits\ForReports\Core\AbstractFormatter;
+use AlecRabbit\Traits\ForReports\Core\Formattable;
+
+class ExtendsAbstractFormatter extends AbstractFormatter
+{
+    public const EXPECTED_CLASS_STUB = 'ExpectedClassStub';
+
+    public function process(Formattable $data): string
+    {
+        return $this->errorMessage($data, self::EXPECTED_CLASS_STUB);
+    }
+}
---
+++ tests/Unit/AbstractFormatterTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Traits;
+
+use AlecRabbit\Traits\ForReports\Core\Formattable;
+use PHPUnit\Framework\TestCase;
+
+class AbstractFormatterTest extends TestCase
+{
+    /** @var HasTraitStoppable */
+    private $obj;
+
+    /** @test */
+    public function check(): void
+    {
+        $str =
+            $this->obj->process(
+                new class extends Formattable
+                {
+                }
+            );
+        $this->assertStringContainsString(ExtendsAbstractFormatter::EXPECTED_CLASS_STUB, $str);
+        $this->assertStringContainsString('expected', $str);
+        $this->assertStringContainsString('given.', $str);
+    }
+
+    protected function setUp(): void
+    {
+        $this->obj = new ExtendsAbstractFormatter();
+    }
+}
